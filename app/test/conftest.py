58d024553f811443f6cf1cf021177abc4f35c762
José Aquiles Guedes de Rezende <jjoseaquiless@gmail.com>
2021-09-04 15:45:07 -0300

    make test database  work in test enviroment
    Co-authored-by: Gabriel Tiveron <gabrielmtiveron@gmail.com>

--- app/test/conftest.py
+++ app/test/conftest.py
@@ -11,18 +11,7 @@ from app.test.utils.overrides import override_get_db
 from app.test.utils.test_db import TestSessionLocal, engine, SQLALCHEMY_TEST_DATABASE_URI
 
 app.dependency_overrides[get_db] = override_get_db
-
-@pytest.fixture(scope="session")
-def set_test_database() -> Generator:
-    if not database_exists(SQLALCHEMY_TEST_DATABASE_URI):
-        create_database(SQLALCHEMY_TEST_DATABASE_URI)
-    Base.metadata.drop_all(bind=engine)
-    Base.metadata.create_all(bind=engine)
-
-    # alembic.config.main(argv=['--autogenerate','revision'])
-    # alembic.config.main(argv=['upgrade','head'])
-    init_db(TestSessionLocal)
-    yield TestSessionLocal
-    if database_exists(SQLALCHEMY_TEST_DATABASE_URI):
-        drop_database(SQLALCHEMY_TEST_DATABASE_URI)
-
+if not database_exists(SQLALCHEMY_TEST_DATABASE_URI):
+    create_database(SQLALCHEMY_TEST_DATABASE_URI)
+Base.metadata.drop_all(bind=engine)
+Base.metadata.create_all(bind=engine)
