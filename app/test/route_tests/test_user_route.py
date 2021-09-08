7973cbd15cf89518258c8d749d4f51799d758aad
José Aquiles Guedes de Rezende <jjoseaquiless@gmail.com>
2021-09-08 17:39:26 -0300

    apply black formatter

--- app/test/route_tests/test_user_route.py
+++ app/test/route_tests/test_user_route.py
@@ -10,73 +10,77 @@ from app.settings import settings
 
 @pytest.fixture(scope="module")
 async def user_created(base_url):
-  user_id = []
-  body = {
-    "email": "barbar@foofoo.com",
-    "first_name": "Foo",
-    "last_name": "bar",
-    "password": "foobar"
-  }  
-  async with AsyncClient(app=app, base_url=base_url) as ac:
-    response = await ac.post('/users/open', json=body)
-  return response
+    user_id = []
+    body = {
+        "email": "barbar@foofoo.com",
+        "first_name": "Foo",
+        "last_name": "bar",
+        "password": "foobar",
+    }
+    async with AsyncClient(app=app, base_url=base_url) as ac:
+        response = await ac.post("/users/open", json=body)
+    return response
+
 
 def test_user_create(user_created):
-  assert user_created.status_code == 200
-  assert list(user_created.json().keys()) == [
-                              'email',
-                              'is_active',
-                              'role',
-                              'first_name',
-                              'last_name',
-                              'id'
-                              ]
+    assert user_created.status_code == 200
+    assert list(user_created.json().keys()) == [
+        "email",
+        "is_active",
+        "role",
+        "first_name",
+        "last_name",
+        "id",
+    ]
+
 
 @pytest.mark.asyncio
 async def test_user_create_auth(base_url, headers):
-  settings.EMAILS_ENABLED = False
-  body = {
-    "email": "user@example.com",
-    "first_name": "string",
-    "last_name": "string",
-    "password": "string"
-  }
-  async with AsyncClient(app=app, base_url=base_url) as ac:
-    response = await ac.post(f'/users/', headers=headers, json=body)
-  assert response.status_code == 200
-  assert list(response.json().keys()) == [
-                              'email',
-                              'is_active',
-                              'role',
-                              'first_name',
-                              'last_name',
-                              'id'
-                              ]
+    settings.EMAILS_ENABLED = False
+    body = {
+        "email": "user@example.com",
+        "first_name": "string",
+        "last_name": "string",
+        "password": "string",
+    }
+    async with AsyncClient(app=app, base_url=base_url) as ac:
+        response = await ac.post(f"/users/", headers=headers, json=body)
+    assert response.status_code == 200
+    assert list(response.json().keys()) == [
+        "email",
+        "is_active",
+        "role",
+        "first_name",
+        "last_name",
+        "id",
+    ]
+
 
 @pytest.mark.asyncio
 async def test_user_get(user_created, headers, base_url):
-  user_json = user_created.json()
-  user_id = user_json["id"]
-  async with AsyncClient(app=app, base_url=base_url) as ac:
-    response = await ac.get(f'/users/{user_id}', headers=headers)
-  assert response.status_code == 200
-  assert response.json() == user_json
+    user_json = user_created.json()
+    user_id = user_json["id"]
+    async with AsyncClient(app=app, base_url=base_url) as ac:
+        response = await ac.get(f"/users/{user_id}", headers=headers)
+    assert response.status_code == 200
+    assert response.json() == user_json
+
 
 @pytest.mark.asyncio
 async def test_user_get_me(headers, base_url):
-  async with AsyncClient(app=app, base_url=base_url) as ac:
-    response = await ac.get(f'/users/me', headers=headers)
-  json = response.json()
-  assert response.status_code == 200
-  assert {
-            "email":settings.FIRST_SUPERUSER,
-            "role":'super_admin'
-  }.items() <= json.items()
-  assert list(json.keys()) == [
-                              'email',
-                              'is_active',
-                              'role',
-                              'first_name',
-                              'last_name',
-                              'id'
-                              ]
+    async with AsyncClient(app=app, base_url=base_url) as ac:
+        response = await ac.get(f"/users/me", headers=headers)
+    json = response.json()
+    assert response.status_code == 200
+    assert {
+        "email": settings.FIRST_SUPERUSER,
+        "role": "super_admin",
+    }.items() <= json.items()
+    assert list(json.keys()) == [
+        "email",
+        "is_active",
+        "role",
+        "first_name",
+        "last_name",
+        "id",
+    ]
