fc2b1c38b256c03b45447a2edaba2d59e9d814c5
José Aquiles Guedes de Rezende <jjoseaquiless@gmail.com>
2021-09-19 22:33:33 -0300

    make creation of dataset and task global.
    other tests will need them.

---
+++ app/test/route_tests/conftest.py
@@ -0,0 +1,47 @@
+import pytest
+from httpx import AsyncClient, Response
+
+from app.main import app
+datasets_body = {
+    "name": "string",
+    "image": "string",
+    "description": "string",
+    "source": "string",
+    "identifier": "string",
+}
+datasets_keys = {*datasets_body, "id"}
+
+
+@pytest.fixture(scope="session")
+async def datasets_created(base_url: str, headers: dict):
+    async with AsyncClient(app=app, base_url=base_url, headers=headers) as ac:
+        response = await ac.post("/datasets", json=datasets_body)
+    yield response
+    datasets_id = response.json()["id"]
+    async with AsyncClient(app=app, base_url=base_url, headers=headers) as ac:
+        response = await ac.delete(f"/datasets/{datasets_id}")
+    assert response.status_code == 200
+    assert dataset_keys == set(response.json().keys())
+
+
+task = {
+    "name": "bar",
+    "image": "foo",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+}
+
+task_keys = {**task, "id": 0, "number_of_benchmarks": 0}
+
+
+@pytest.fixture(scope="session")
+async def task_created(base_url: str, headers: dict):
+    async with AsyncClient(app=app, base_url=base_url, headers=headers) as ac:
+        response = await ac.post("/tasks", json=task)
+    a = response.json()
+    assert response.status_code == 200
+    assert response.json().keys() == a.keys()
+    yield response
+    async with AsyncClient(app=app, base_url=base_url, headers=headers) as ac:
+        response = await ac.delete(f"/tasks/{a['id']}")
+    assert response.status_code == 200
+    assert response.json().keys() == task_keys.keys()
--- app/test/route_tests/test_datasets_route.py
+++ app/test/route_tests/test_datasets_route.py
@@ -2,34 +2,13 @@ import pytest
 from httpx import AsyncClient, Response
 
 from app.main import app
-
-post_body = {
-    "name": "string",
-    "image": "string",
-    "description": "string",
-    "source": "string",
-    "identifier": "string"
-}
-keys = {*post_body, "id"}
-
-
-@pytest.fixture(scope="module")
-async def datasets_created(base_url: str, headers: dict):
-    async with AsyncClient(app=app, base_url=base_url, headers=headers) as ac:
-        response = await ac.post("/datasets", json=post_body)
-    yield response
-    datasets_id = response.json()["id"]
-    async with AsyncClient(app=app, base_url=base_url, headers=headers) as ac:
-        response = await ac.delete(f"/datasets/{datasets_id}")
-    assert response.status_code == 200
-    assert keys == set(response.json().keys())
+from .conftest import datasets_body, datasets_keys
 
 
 def test_datasets_post(datasets_created: Response):
     assert datasets_created.status_code == 200
-    assert datasets_created.json().items() >= post_body.items()
-    assert set(datasets_created.json().keys()) == keys
-
+    assert datasets_created.json().items() >= datasets_body.items()
+    assert set(datasets_created.json().keys()) == datasets_keys
 
 
 @pytest.mark.asyncio
@@ -37,7 +16,7 @@ async def test_datasets_get(headers: dict, base_url: str, datasets_created: Resp
     async with AsyncClient(app=app, base_url=base_url) as ac:
         response = await ac.get(f"/datasets/?skip=0&limit=1", headers=headers)
     assert response.status_code == 200
-    assert set(response.json()[0].keys()) == keys
+    assert set(response.json()[0].keys()) == datasets_keys
 
 
 @pytest.mark.asyncio
@@ -57,9 +36,9 @@ async def test_datasets_put(base_url: str, headers: dict, datasets_created: Resp
     datasets_json = datasets_created.json()
     datasets_id = datasets_json["id"]
     put_json = {
-        **post_body,
+        **datasets_body,
         "description": "i changed my description because i want",
-        "source": "example"
+        "source": "example",
     }
     async with AsyncClient(app=app, base_url=base_url, headers=headers) as ac:
         response = await ac.put(f"/datasets/{datasets_id}", json=put_json)
