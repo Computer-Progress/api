242c1468745f82c99fe1d33b58971ebfebfd24be
José Aquiles Guedes de Rezende <jjoseaquiless@gmail.com>
2021-10-17 19:32:13 -0300

    test paper get endpoint

--- app/test/route_tests/test_paper_route.py
+++ app/test/route_tests/test_paper_route.py
@@ -14,3 +14,18 @@ def test_paper_post(paper_created: Response) -> None:
     assert paper_created.status_code == SUCCESS
     assert paper_created.json().items() >= PAPER_GET.items()
     assert set(paper_created.json().keys()) == PAPER_KEYS_POST
+
+@pytest.mark.asyncio
+async def test_paper_get(headers: dict, base_url: str):
+    async with AsyncClient(app=app, base_url=base_url) as ac:
+        response = await ac.get("/papers/?skip=0&limit=1", headers=headers)
+    assert response.status_code == SUCCESS
+    assert set(response.json()[0].keys()) == PAPER_KEYS
+
+@pytest.mark.asyncio
+async def test_paper_get_id(base_url: str, headers: dict, paper_created: Response):
+    paper_id = paper_created.json()["id"]
+    async with AsyncClient(app=app, base_url=base_url, headers=headers) as ac:
+        response = await ac.get(f"/papers/{paper_id}")
+    assert response.status_code == SUCCESS
+    assert { **PAPER_GET, "id": paper_id} == response.json()
