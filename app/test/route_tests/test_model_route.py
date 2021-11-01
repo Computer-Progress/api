635062489561788b85e7dd34726c72560218f3e1
José Aquiles Guedes de Rezende <jjoseaquiless@gmail.com>
2021-10-31 21:01:52 -0300

    test model put route

--- app/test/route_tests/test_model_route.py
+++ app/test/route_tests/test_model_route.py
@@ -13,10 +13,12 @@ async def test_model_get_id(headers: dict, base_url: str, get_test_model: Respon
     assert response.status_code == SUCCESS
     assert get_test_model == response.json()
 
+
 @pytest.mark.asyncio
-async def test_model_get_id_csv(headers: dict, base_url: str, get_test_model: Response):
+async def test_model_put(headers: dict, base_url: str, get_test_model: Response):
     model_id = get_test_model["id"]
+    put_body = {**get_test_model, "name": "fooo", "gflops": 2, "epochs": 3}
     async with AsyncClient(app=app, base_url=base_url, headers=headers) as ac:
-        response = await ac.get(f"/models/{model_id}")
+        response = await ac.put(f"/models/{model_id}", json=put_body)
     assert response.status_code == SUCCESS
-    assert get_test_model == response.json()
+    assert set(put_body.items()) <= set(response.json().items())
